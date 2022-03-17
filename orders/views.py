fc3fd4bfac24d8f6dc3a839b0fba84df927e38c0
Yusuf <yustaoab@gmail.com>
2022-03-17 18:26:34 +0100

    k

--- orders/views.py
+++ orders/views.py
@@ -266,6 +266,24 @@ def showQwikAdminOrders(request):
 
 @login_required
 @permission_required('users.view_admin')
+def showQwikAdminCredits(request):
+    context = {}
+    filtered_orders = UserOrderFilter2(
+        request.GET,
+        queryset = UserOrder.objects.all()
+    )
+    context['filtered_orders'] = filtered_orders
+    paginated_filtered_orders = Paginator(filtered_orders.qs, 10)
+    page_number = request.GET.get('page')
+    orders_page_obj = paginated_filtered_orders.get_page(page_number)
+    context['orders_page_obj'] = orders_page_obj
+    total_orders = filtered_orders.qs.count()
+    context['total_orders'] = total_orders
+
+    return render(request, 'orders/qwikadmin_credits.html', context=context)
+
+@login_required
+@permission_required('users.view_admin')
 def showQwikAdminSales(request):
     context = {}
     filtered_orders = UserOrderFilter2(
